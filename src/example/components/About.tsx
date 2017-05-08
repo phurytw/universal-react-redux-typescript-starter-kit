6a6ba52e6774a54759354bf42c7b21321a38d6a1
François Nguyen <lithos2406@live.fr>
2017-05-08 14:03:55 +0200

    build script fix

--- src/example/components/About.tsx
+++ src/example/components/About.tsx
@@ -5,8 +5,11 @@ export default (props: RouteComponentProps<void>) => (
     <div>
         <h2>About</h2>
         <p>
-            Find more about this starter kit on
-        <a href="https://github.com/lith-light-g/universal-react-redux-typescript-starter-kit">GitHub</a>.
+            Find more about this starter kit on <a
+                href="https://github.com/lith-light-g/universal-react-redux-typescript-starter-kit"
+            >
+                GitHub
+            </a>.
     </p>
     </div>
 );
